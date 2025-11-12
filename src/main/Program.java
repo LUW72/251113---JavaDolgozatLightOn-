8df27d54d21497d3757880e2a1155f3a71243a2a
Feller Dénes (Szf_N_2024_2026) <feller.denes@diak.szamalk-szalezi.hu>
2025-11-12 14:38:09 +0100

    GUI terv letrehozasa, teszt kepek csatolasa

--- src/main/Program.java
+++ src/main/Program.java
@@ -4,7 +4,6 @@ package main;
 public class Program 
 {
 
-
     public static void main(String[] args) 
     {
         
--- src/vezerlo/LampakVezerlo.java
+++ src/vezerlo/LampakVezerlo.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vezerlo;
 
-/**
- *
- * @author FellerDénes(Szf_N_20
- */
-public class LampakVezerlo {
+public class LampakVezerlo 
+{
     
 }
