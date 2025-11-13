b1efa15af5c0d560b0e0dddbc12a22ba74d050e0
LUW72 <schneider.ivan337@gmail.com>
2025-11-13 13:35:40 +0100

    ujraindit gomb, mentes gomb megirasa

--- src/vezerlo/LampakVezerlo.java
+++ src/vezerlo/LampakVezerlo.java
@@ -1,6 +1,13 @@
 package vezerlo;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import modell.LampakModell;
 import nezet.GUINezetLampak;
 
@@ -21,12 +28,32 @@ public class LampakVezerlo
     
     public void init()
     {
+        jatekterInit();
+        
         nezet.getJbtnUjra().addActionListener((e) -> {
             nezet.setJlblJatekAllas("A játék újraindult");
+            ujraindit();
+            jatekterInit();            
+        });
+        
+        nezet.getJmItemKilepes().addActionListener((e)->{
+            int valasz = JOptionPane.showConfirmDialog(null, "Biztosan kilépsz?", "Kilépés", JOptionPane.YES_NO_OPTION);
+            if (valasz == JOptionPane.YES_OPTION) 
+            {
+                System.exit(0);
+            }
+        });
+
+        nezet.getJmItemBetoltes().addActionListener((e)->{
+            
+        });
+        
+        nezet.getJmItemMentes().addActionListener((e)->{
+            mentesFajlba();
         });
     }
     
-    public void lampaListaInit()
+    private void lampaListaInit()
     {
         this.lampaLista[0] = nezet.getJbtnLampa0();
         this.lampaLista[1] = nezet.getJbtnLampa1();
@@ -38,6 +65,59 @@ public class LampakVezerlo
         this.lampaLista[7] = nezet.getJbtnLampa7();
         this.lampaLista[8] = nezet.getJbtnLampa8();        
     }    
+
+    private void jatekterInit() 
+    {
+        for (int i = 0; i < modell.getLampak().length; i++)
+        {
+            lampaIkonBeallit(i);
+        }        
+    }
+
+    private void lampaIkonBeallit(int i) 
+    {
+        if(modell.getLampak()[i].isAllapot())
+        {
+            // felkapcsolt
+            lampaLista[i].setIcon(new javax.swing.ImageIcon(getClass().getResource("/nezet/lampa_sarga.png")));
+        }
+        else
+        {
+            // lekapcsolt
+            lampaLista[i].setIcon(new javax.swing.ImageIcon(getClass().getResource("/nezet/lampa_zold.png")));
+        }        
+    }
     
+    private void ujraindit()
+    {
+        this.modell = new LampakModell();
+    }
+
+    private void mentesFajlba() 
+    {
+        String sorok = "";
+        
+        for (int i = 0; i < modell.getLampak().length; i++) 
+            {
+                String allapot = modell.getLampak()[i].isAllapot() ? "1" : "0";
+                System.out.println(allapot);
+                sorok += allapot;
+            }
+        
+        try 
+        {
+            Files.write(Paths.get("mentes.txt"), sorok.getBytes(StandardCharsets.UTF_8));
+            nezet.setJlblJatekAllas("Játék mentve!");
+        } 
+        catch (IOException e) 
+        {
+            nezet.setJlblJatekAllas("Hiba a mentés közben: " + e.getMessage());
+        }
+    }
+    
+    private void betoltesFajlbol()
+    {
+        
+    }
     
 }
